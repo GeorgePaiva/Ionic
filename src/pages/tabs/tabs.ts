a54d46a3e36b40b13bf3186535840f608ebc6a97
George Paiva <george.paiva15@gmail.com>
2018-06-13 12:05:04 -0300

    first commit

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { HomePage } from '../home/home';
+import { FeedPage } from '../feed/feed';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab4Root = FeedPage;
+
+  constructor() {
+
+  }
+}
